bcbad2b66b24637534d4e6821decee18f547e9c5
zhangying <18992937865@163.com>
2021-06-07 09:12:13 +0800

    "第一次提交"

---
+++ src/main/java/com/baizhi/Gittest01Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gittest01Application.class, args);
+    }
+
+}
